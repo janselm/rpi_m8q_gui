ce45a99f4b0b307419c6c7f69f1f007a7bf5db3f
janselm <janselm@asu.edu>
2025-05-04 14:29:46 -0400

    replaced drawn circles with png files

--- guiTest/gui_setup.c
+++ guiTest/gui_setup.c
@@ -39,6 +39,7 @@ atomic_bool *guiRunning;
 
 // declare the utc offset variable
 int utcOffset = 0; 
+static bool isRed = true;
 
 /**
  * applies the appropriate utc offset when the user selects the time zone
@@ -164,20 +165,25 @@ void initGUI() {
   gtk_box_pack_start(GTK_BOX(rightVBox), guiWindow.primaryAirLabel, FALSE, FALSE, 0);
 
   // Red circle (Primary Air)
-  guiWindow.primaryAirCircle = gtk_drawing_area_new();
-  gtk_widget_set_size_request(guiWindow.primaryAirCircle, 50, 50);
-  g_signal_connect(G_OBJECT(guiWindow.primaryAirCircle), "draw", G_CALLBACK(draw_red_circle), NULL);
+  guiWindow.primaryAirCircle = gtk_image_new_from_file("red_circle.png");
   gtk_box_pack_start(GTK_BOX(rightVBox), guiWindow.primaryAirCircle, FALSE, FALSE, 0);
+  
+  // Add click interaction to toggle color
+  gtk_widget_set_events(guiWindow.primaryAirCircle, GDK_BUTTON_PRESS_MASK);
+  g_signal_connect(G_OBJECT(guiWindow.primaryAirCircle), "button-press-event", G_CALLBACK(on_circle_clicked), NULL);  
 
   // Secondary Air label
   guiWindow.secondaryAirLabel = gtk_label_new("Secondary Air");
   gtk_box_pack_start(GTK_BOX(rightVBox), guiWindow.secondaryAirLabel, FALSE, FALSE, 0);
 
   // Red circle (Secondary Air)
-  guiWindow.secondaryAirCircle = gtk_drawing_area_new();
-  gtk_widget_set_size_request(guiWindow.secondaryAirCircle, 50, 50);
-  g_signal_connect(G_OBJECT(guiWindow.secondaryAirCircle), "draw", G_CALLBACK(draw_red_circle), NULL);
+  guiWindow.secondaryAirCircle = gtk_image_new_from_file("red_circle.png");
   gtk_box_pack_start(GTK_BOX(rightVBox), guiWindow.secondaryAirCircle, FALSE, FALSE, 0);
+  
+  // Add click interaction to toggle color
+  gtk_widget_set_events(guiWindow.secondaryAirCircle, GDK_BUTTON_PRESS_MASK);
+  g_signal_connect(G_OBJECT(guiWindow.secondaryAirCircle), "button-press-event", G_CALLBACK(on_circle_clicked), NULL);
+  
 
   // Spacer at the bottom for vertical centering
   GtkWidget *bottomSpacer = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
@@ -196,48 +202,30 @@ void initGUI() {
 
 }
 
+gboolean on_circle_clicked(GtkWidget *widget, GdkEventButton *event, gpointer user_data) {
+  static bool isPrimaryRed = true;
+  static bool isSecondaryRed = true;
 
+  if (widget == guiWindow.primaryAirCircle) {
+    if (isPrimaryRed) {
+      gtk_image_set_from_file(GTK_IMAGE(guiWindow.primaryAirCircle), "green_circle.png");
+    } else {
+      gtk_image_set_from_file(GTK_IMAGE(guiWindow.primaryAirCircle), "red_circle.png");
+    }
+    isPrimaryRed = !isPrimaryRed;
 
-// void initGUI() {
-//   // Initialize GUI elements
-//   guiWindow.window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-//   gtk_window_set_title(GTK_WINDOW(guiWindow.window), "GPS Data Display");
-//   gtk_window_set_default_size(GTK_WINDOW(guiWindow.window), 300, 200);
+  } else if (widget == guiWindow.secondaryAirCircle) {
+    if (isSecondaryRed) {
+      gtk_image_set_from_file(GTK_IMAGE(guiWindow.secondaryAirCircle), "green_circle.png");
+    } else {
+      gtk_image_set_from_file(GTK_IMAGE(guiWindow.secondaryAirCircle), "red_circle.png");
+    }
+    isSecondaryRed = !isSecondaryRed;
+  }
 
-//   // Create labels for GPS data
-//   guiWindow.timeLabel = gtk_label_new("");  // Time label without prefix
-//   guiWindow.latitudeLabel = gtk_label_new("LAT: ");
-//   guiWindow.longitudeLabel = gtk_label_new("LON: ");
-//   guiWindow.speedLabel = gtk_label_new("Speed: ");  // Speed label
-//   guiWindow.timeZoneDropdown = gtk_combo_box_text_new();
-//   guiWindow.closeButton = gtk_button_new_with_label("CLOSE");
-//   gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(guiWindow.timeZoneDropdown), "Eastern Standard Time (EST)");
-//   gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(guiWindow.timeZoneDropdown), "Central Standard Time (CST)");
-//   gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(guiWindow.timeZoneDropdown), "Mountain Standard Time (MST)");
-//   gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(guiWindow.timeZoneDropdown), "Pacific Standard Time (PST)");
-//   gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(guiWindow.timeZoneDropdown), "Alaska Standard Time (AKST)");
-//   gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(guiWindow.timeZoneDropdown), "Hawaii-Aleutian Standard Time (HAST)");
+  return TRUE;
+}
 
-//   // Layout
-//   GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
-//   gtk_container_add(GTK_CONTAINER(guiWindow.window), vbox);
-
-//   gtk_box_pack_start(GTK_BOX(vbox), guiWindow.timeZoneDropdown, TRUE, TRUE, 0);
-//   gtk_box_pack_start(GTK_BOX(vbox), guiWindow.timeLabel, TRUE, TRUE, 0);  // Pack time label
-//   gtk_box_pack_start(GTK_BOX(vbox), guiWindow.latitudeLabel, TRUE, TRUE, 0);  // Pack latitude label
-//   gtk_box_pack_start(GTK_BOX(vbox), guiWindow.longitudeLabel, TRUE, TRUE, 0);  // Pack longitude label
-//   gtk_box_pack_start(GTK_BOX(vbox), guiWindow.speedLabel, TRUE, TRUE, 0);  // Pack speed label
-//   gtk_box_pack_start(GTK_BOX(vbox), guiWindow.closeButton, TRUE, TRUE, 0);
-
-//   // Show all widgets
-//   gtk_widget_show_all(guiWindow.window);
-  
-//   g_signal_connect(G_OBJECT(guiWindow.timeZoneDropdown), "changed", G_CALLBACK(on_time_zone_changed), NULL);
-
-//   // Connect the destroy signal (when the window is closed)
-//   g_signal_connect(guiWindow.window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-//   g_signal_connect(G_OBJECT(guiWindow.closeButton), "clicked", G_CALLBACK(on_close_button_clicked), NULL);
-// }
 
 
 void *startGUI(void *arg) {
